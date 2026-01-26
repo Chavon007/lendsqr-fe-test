c9206c354b92aa356914de2bd1eb4aef7fd83376
azuh salvation <salvationazuh@gmail.com>
2026-01-26 17:35:10 +0100

    update

---
+++ src/types/user.tsx
@@ -0,0 +1,45 @@
+export type UserStatus = "Active" | "Inactive" | "Pending" | "Blacklisted";
+
+export interface User {
+  id: string;
+  fullName: string;
+  username: string;
+  email: string;
+  phoneNumber: string;
+  bvn: string;
+  status: UserStatus;
+  tier: 1 | 2 | 3;
+
+  accountBalance: string;
+  accountNumber: string;
+  bankName: string;
+
+  personalInfo: {
+    maritalStatus: string;
+    children: string;
+    residenceType: string;
+  };
+
+  educationEmployment: {
+    level: string;
+    employmentStatus: string;
+    sector: string;
+    duration: string;
+    officeEmail: string;
+    monthlyIncome: string;
+    loanRepayment: string;
+  };
+
+  socials: {
+    twitter: string;
+    facebook: string;
+    instagram: string;
+  };
+
+  guarantors: {
+    fullName: string;
+    phoneNumber: string;
+    email: string;
+    relationship: string;
+  }[];
+}
